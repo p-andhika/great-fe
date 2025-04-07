3f5f682782f67d6834cfa5b98dff91c42ca39c51
Andhika Prakasiwi <p.andhika@op.iitg.ac.in>
2025-04-07 11:17:31 +0700

    feat: implement basic jQuery-like css function

--- week/7_jquery.js
+++ week/7_jquery.js
@@ -29,5 +29,24 @@ Implement the jQuery single-character function $ which only needs to supports th
  * @return {{css: Function}}
  */
 export default function $(selector) {
-  throw "Not implemented!";
+  const element = document.querySelector(selector);
+
+  return {
+    css: function (prop, value) {
+      if (value === undefined) {
+        if (element === null) {
+          return undefined;
+        }
+
+        const value = element.style[prop];
+        return value === "" ? undefined : value;
+      }
+
+      if (element != null) {
+        element.style[prop] = value;
+      }
+
+      return this;
+    },
+  };
 }
